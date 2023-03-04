f2d9d17b26b7a59be1476c6976b8db5b51c504d1
CrackTC <chenrke369@gmail.com>
2023-03-04 11:23:16 +0800

    update inventory.h

--- data/inventory.h
+++ data/inventory.h
@@ -9,29 +9,93 @@
 
 #include "time.h"
 #include "amount.h"
+#include "linkedList.h"
 
 /*
  * Name: InventoryEntry
  * Description: 表示一个库存条目
  * */
-struct InventoryEntry {
-    // 库存编号
-    int id;
+struct InventoryEntry;
+/*
+ * Name: GetAllInventory
+ * Description: 获取所有库存条目
+ * Returns: 一个链表，包含所有库存条目
+ *          若不存在库存条目，返回NULL
+ * */
+const LinkedListPointer GetAllInventory();
 
-    // 商品编号
-    int itemId;
+/*
+ * Name: GetInventoryById
+ * Description: 获取具有指定库存编号的库存条目
+ * Arguments:
+ *     id: 指定的库存编号
+ * Returns: 符合条件的库存条目
+ *          若不存在符合条件的条目，返回NULL
+ * */
+const InventoryEntry *GetInventoryById(int id);
 
-    // 数量
-    int number;
+/*
+ * Name: GetInventoryByItemId
+ * Description: 获取具有指定商品编号的库存条目
+ * Arguments:
+ *     itemId: 指定的商品编号
+ * Returns: 一个链表，包含所有符合条件的库存条目
+ *          若不存在符合条件的条目，返回NULL
+ * */
+const LinkedListPointer GetInventoryByItemId(int itemId);
 
-    // 入库时间
-    Time *inboundTimePointer;
+/*
+ * Name: NewInventoryEntry
+ * Description: 创建一个新的库存条目
+ * Arguments:
+ *     destination: 指向用于存放创建结果的Inventory对象的指针
+ *     itemId: 商品编号
+ *     number: 数量
+ *     inboundTimePointer: 指向入库时间的指针
+ *     productionTimePointer: 指向生产日期的指针
+ *     inboundUnitPricePointer: 指向入库单价的指针
+ * Returns: 若成功创建，返回0
+ *          否则，返回1
+ * */
+int NewInventoryEntry(
+        InventoryEntry *destination,
+        int itemId,
+        int number,
+        const Time *inboundTimePointer,
+        const Time *productionTimePointer,
+        const Amount *inboundUnitPricePointer);
 
-    // 生产日期
-    Time *productionTimePointer;
+/*
+ * Name: GetInventoryEntryXXX
+ * Description: 获取库存条目的某个成员
+ * Arguments:
+ *     entry: 指向InventoryEntry的指针
+ * Returns: 对应的值
+ * */
+int GetInventoryEntryId(const InventoryEntry *entry);
+int GetInventoryEntryItemId(const InventoryEntry *entry);
+int GetInventoryEntryNumber(const InventoryEntry *entry);
+Time *GetInventoryEntryInboundTimePointer(const InventoryEntry *entry);
+Time *GetInventoryEntryProductionTimePointer(const InventoryEntry *entry);
+Amount *GetInventoryEntryInboundUnitPricePointer(const InventoryEntry *entry);
 
-    // 购入单价
-    Amount *inboundUnitPricePointer;
-};
+/*
+ * Name: SetInventoryEntryXXX
+ * Description: 设置库存条目的某个成员
+ * Arguments:
+ *     entry: 指向要设置成员的库存条目的指针
+ *     value: 要设置成的值
+ * */
+void SetInventoryEntryItemId(InventoryEntry *entry, int value);
+void SetInventoryEntryNumber(InventoryEntry *entry, int value);
+void SetInventoryEntryInboundTimePointer(InventoryEntry *entry, Time *value);
+void SetInventoryEntryProductionTimePointer(InventoryEntry *entry, Time *value);
+void SetInventoryEntryInboundUnitPricePointer(InventoryEntry *entry, Amount *value);
+
+/*
+ * Name: InventorySave
+ * Description: 保存对库存条目的所有更改
+ * */
+void InventorySave();
 
 #endif
