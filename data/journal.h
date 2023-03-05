13187d7af485ef9ccdd1c4c62635b13cd58182e3
CrackTC <chenrke369@gmail.com>
2023-03-05 20:52:03 +0800

    add journal.h

---
+++ data/journal.h
@@ -0,0 +1,95 @@
+/*
+ * FileName: data/journal.h
+ * Author: 陈若珂
+ * Description: 包含日志条目的相关定义和声明
+ * */
+
+#ifndef JOURNAL_H
+#define JOURNAL_H
+
+#include "time.h"
+#include "operation.h"
+#include "linkedList.h"
+
+/*
+ * Name: JournalEntry
+ * Description: 表示一个日志条目
+ * */
+typedef struct JournalEntry JournalEntry;
+
+/*
+ * Name: NewJournalEntry
+ * Description: 创建一个新的日志条目
+ * Arguments:
+ *     staffId: 员工编号
+ *     time: 时间
+ *     operation: 操作
+ *     arguments: 操作相关参数
+ * */
+JournalEntry *NewJournalEntry(int staffId, Time *time, Operation operation, const char **arguments);
+
+/*
+ * Name: FreeJournalEntry
+ * Description: 释放指定的JournalEntry占用的空间
+ * Arguments:
+ *     entry: 一个指针，指向要释放空间的JournalEntry对象
+ * */
+void FreeJournalEntry(JournalEntry *entry);
+
+/*
+ * Name: GetAllJournals
+ * Description: 获取所有日志条目
+ * Returns: 一个链表，包含所有日志条目
+ *          若不存在日志条目，返回NULL
+ * */
+const LinkedList *GetAllJournals();
+
+/*
+ * Name: GetJournalsByStaffId
+ * Description: 获取具有指定员工编号的所有日志条目
+ * Arguments:
+ *     staffId: 指定的员工编号
+ * Returns: 一个链表，包含所有符合条件的日志条目
+ *          若不存在这样的日志条目，返回NULL
+ * */
+const LinkedList *GetJournalsByStaffId(int staffId);
+
+/*
+ * Name: GetJournalsByOperation
+ * Description: 获取具有指定操作的所有日志条目
+ * Arguments:
+ *     operation: 指定的操作
+ * Returns: 一个链表，包含所有符合条件的日志条目
+ *          若不存在这样的日志条目，返回NULL
+ * */
+const LinkedList *GetJournalsByOperation(Operation operation);
+
+/*
+ * Name: GetJournalEntryXXX
+ * Description: 获取日志条目的某个成员
+ * Arguments:
+ *     entry: 指向JournalEntry的指针
+ * Returns: 对应的值
+ * */
+int GetJournalEntryStaffId(const JournalEntry *entry);
+Time *GetJournalEntryTime(const JournalEntry *entry);
+Operation GetJournalEntryOperation(const JournalEntry *entry);
+const char **GetJournalEntryArguments(const JournalEntry *entry);
+
+/*
+ * Name: AppendJournalEntry
+ * Description: 向系统中追加一个JournalEntry对象
+ * Arguments:
+ *     entry: 一个指针，指向要追加的对象
+ * Returns: 若成功追加，返回0
+ *          否则，返回1
+ * */
+int AppendJournalEntry(JournalEntry *entry);
+
+/*
+ * Name: JournalSave
+ * Description: 保存对日志条目的所有更改
+ * */
+void JournalSave();
+
+#endif
