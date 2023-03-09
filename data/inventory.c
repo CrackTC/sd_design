227b33d525811db7143bc8969d2ec3d7b9962d39
CrackTC <chenrke369@gmail.com>
2023-03-09 22:33:12 +0800

    update inventory.c

--- data/inventory.c
+++ data/inventory.c
@@ -9,3 +9,100 @@ struct InventoryEntry
     const Time *productionTime;
     const Amount *inboundUnitPrice;
 };
+
+const LinkedList *GetAllInventory()
+{
+
+}
+
+const InventoryEntry *GetInventoryById(int id)
+{
+
+}
+
+LinkedList *GetInventoryByItemId(int itemId)
+{
+
+}
+
+InventoryEntry *NewInventoryEntry(int itemId, int number, const Time *inboundTime, const Time *productionTime,
+                                  const Amount *inboundUnitPrice)
+{
+
+}
+
+void FreeInventoryEntry(InventoryEntry *entry)
+{
+
+}
+
+
+int GetInventoryEntryId(const InventoryEntry *entry)
+{
+
+}
+
+int GetInventoryEntryItemId(const InventoryEntry *entry)
+{
+
+}
+
+int GetInventoryEntryNumber(const InventoryEntry *entry)
+{
+
+}
+
+const Time *GetInventoryEntryInboundTimePointer(const InventoryEntry *entry)
+{
+
+}
+
+const Time *GetInventoryEntryProductionTimePointer(const InventoryEntry *entry)
+{
+
+}
+
+const Amount *GetInventoryEntryInboundUnitPricePointer(const InventoryEntry *entry)
+{
+
+}
+
+void SetInventoryEntryItemId(InventoryEntry *entry, int value)
+{
+
+}
+
+void SetInventoryEntryNumber(InventoryEntry *entry, int value)
+{
+
+}
+
+void SetInventoryEntryInboundTimePointer(InventoryEntry *entry, const Time *value)
+{
+
+}
+
+void SetInventoryEntryProductionTimePointer(InventoryEntry *entry, const Time *value)
+{
+
+}
+
+void SetInventoryEntryInboundUnitPricePointer(InventoryEntry *entry, const Amount *value)
+{
+
+}
+
+int AppendInventoryEntry(InventoryEntry *entry)
+{
+
+}
+
+int RemoveInventoryEntry(InventoryEntry *entry)
+{
+
+}
+
+void InventorySave()
+{
+
+}
--- data/inventory.h
+++ data/inventory.h
@@ -78,9 +78,9 @@ void FreeInventoryEntry(InventoryEntry *entry);
 int GetInventoryEntryId(const InventoryEntry *entry);
 int GetInventoryEntryItemId(const InventoryEntry *entry);
 int GetInventoryEntryNumber(const InventoryEntry *entry);
-Time *GetInventoryEntryInboundTimePointer(const InventoryEntry *entry);
-Time *GetInventoryEntryProductionTimePointer(const InventoryEntry *entry);
-Amount *GetInventoryEntryInboundUnitPricePointer(const InventoryEntry *entry);
+const Time *GetInventoryEntryInboundTimePointer(const InventoryEntry *entry);
+const Time *GetInventoryEntryProductionTimePointer(const InventoryEntry *entry);
+const Amount *GetInventoryEntryInboundUnitPricePointer(const InventoryEntry *entry);
 
 /*
  * Name: SetInventoryEntryXXX
@@ -91,9 +91,9 @@ Amount *GetInventoryEntryInboundUnitPricePointer(const InventoryEntry *entry);
  * */
 void SetInventoryEntryItemId(InventoryEntry *entry, int value);
 void SetInventoryEntryNumber(InventoryEntry *entry, int value);
-void SetInventoryEntryInboundTimePointer(InventoryEntry *entry, Time *value);
-void SetInventoryEntryProductionTimePointer(InventoryEntry *entry, Time *value);
-void SetInventoryEntryInboundUnitPricePointer(InventoryEntry *entry, Amount *value);
+void SetInventoryEntryInboundTimePointer(InventoryEntry *entry, const Time *value);
+void SetInventoryEntryProductionTimePointer(InventoryEntry *entry, const Time *value);
+void SetInventoryEntryInboundUnitPricePointer(InventoryEntry *entry, const Amount *value);
 
 /*
  * Name: AppendInventoryEntry
