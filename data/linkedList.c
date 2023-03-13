c80dee28ebe3c546b45a795391e7c2a7131e8948
CrackTC <chenrke369@gmail.com>
2023-03-13 14:52:12 +0800

    finish linkedList.c

---
+++ data/linkedList.c
@@ -0,0 +1,70 @@
+#include "linkedList.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+LinkedList *GetNodeByIndex(LinkedList *head, int index)
+{
+    if (index < 0)
+    {
+        return NULL;
+    }
+    if (index == 0)
+    {
+        return head;
+    }
+    return GetNodeByIndex(head->next, index - 1);
+}
+
+LinkedList *GetLastNode(LinkedList *head)
+{
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->next == NULL) {
+        return head;
+    }
+    return GetLastNode(head->next);
+}
+
+int ExistsNode(LinkedList *list, const void *node)
+{
+    if (list == NULL) {
+        return 0;
+    }
+    if (list->data == node) {
+        return 1;
+    }
+    return ExistsNode(list->next, node);
+}
+
+LinkedList *AppendNode(LinkedList *head, LinkedList *node)
+{
+    node->next = head;
+    return node;
+}
+
+LinkedList *RemoveNode(LinkedList *head, LinkedList *node)
+{
+    if (node == NULL) {
+        return head;
+    }
+    if (head == node)
+        return head->next;
+    
+    LinkedList *now = head;
+    while (now != NULL) {
+        if (now->next == node) {
+            now->next = now->next->next;
+            return head;
+        }
+        now = now->next;
+    }
+    return head;
+}
+
+void FreeList(LinkedList *list)
+{
+    if (list->next != NULL)
+        FreeList(list->next);
+    free(list);
+}
