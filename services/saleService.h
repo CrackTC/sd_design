e9288d3e197645cd80cfd16778bb5947d41b1b6f
CrackTC <chenrke369@gmail.com>
2023-03-15 21:47:01 +0800

    fix include path and format

--- services/saleService.h
+++ services/saleService.h
@@ -1,7 +1,7 @@
 #ifndef SALE_H
 #define SALE_H
 
-#include "table.h"
+#include "../data/table.h"
 
 /*
  * Name: AddRefund
@@ -15,46 +15,46 @@
  * Returns: 若退货成功，返回0
  *          否则，返回1
  * */
-//int AddRefund(int orderId, const char *reason,int yuan,int jiao,int fen,int number,const char *remark);
-//添加退货
+// int AddRefund(int orderId, const char *reason,int yuan,int jiao,int fen,int number,const char *remark);
+// 添加退货
 Table *AddRefund(Table *a);
 
-//删除退货
+// 删除退货
 Table *RemoveRefund(Table *a);
 
-//修改退货
+// 修改退货
 Table *UpdateRefund(Table *a);
-//Table *ResetReason(Table *a);
-//Table *ResetMoney(Table *a);
-//Table *ResetNumber(Table *a);
-//Table *ResetRemark(Table *a);
+// Table *ResetReason(Table *a);
+// Table *ResetMoney(Table *a);
+// Table *ResetNumber(Table *a);
+// Table *ResetRemark(Table *a);
 
-//查询退货
+// 查询退货
 Table *GetAllRefunds(Table *a);
 Table *GetSingleRefund(Table *a);
 
-//读取订单
+// 读取订单
 Table *GetAllOrders(Table *a);
 Table *GetSingleOrder(Table *a);
 
-//添加折扣
+// 添加折扣
 Table *AddDiscount(Table *a);
 
-//删除折扣
+// 删除折扣
 Table *RemoveDiscount(Table *a);
 
-//更新折扣
+// 更新折扣
 Table *UpdateDiscount(Table *a);
 
-//查询折扣
+// 查询折扣
 Table *GetAllDiscount(Table *a);
 Table *GetSingleDiscount(Table *a);
 
-//添加售货
-Table *AddOrder(Table *a);//需要商品id,客户id,number//自动选择出货批次
-Table *AddSpecificOrder(Table *a);//需要商品id,客户id,number,指定库存编号//手动选择出货批次
+// 添加售货
+Table *AddOrder(Table *a);         // 需要商品id,客户id,number//自动选择出货批次
+Table *AddSpecificOrder(Table *a); // 需要商品id,客户id,number,指定库存编号//手动选择出货批次
 
-//赠送商品
-Table *Happy(Table *a);//需要商品id,客户id,number
+// 赠送商品
+Table *Happy(Table *a); // 需要商品id,客户id,number
 
 #endif // SALE_H
--- services/statisticService.h
+++ services/statisticService.h
@@ -1,9 +1,9 @@
 #ifndef STATISTIC_H
 #define STATISTIC_H
 
-#include "table.h"
+#include "../data/table.h"
 
-//读取所有的收支条目并计算总金额、输出总金额
+// 读取所有的收支条目并计算总金额、输出总金额
 Table *GetStatistics(Table *a);
 
 #endif // STATISTIC_H
