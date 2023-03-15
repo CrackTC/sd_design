e64a12b5344ae328f1efa7758fa8b02a0049ca24
CrackTC <chenrke369@gmail.com>
2023-03-15 23:56:08 +0800

    add judgeService.h

---
+++ services/judgeService.h
@@ -0,0 +1,8 @@
+#ifndef JUDGE_H
+#define JUDGE_H
+#include "../data/table.h"
+#include "../data/operation.h"
+
+Table *judge(int staffId, const char *password, Operation operation, Table *a);
+
+#endif
