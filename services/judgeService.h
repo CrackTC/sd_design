908582529beda38282f3380a236621906ef1defc
CrackTC <chenrke369@gmail.com>
2023-03-18 23:02:34 +0800

    fix format

--- services/judgeService.h
+++ services/judgeService.h
@@ -1,7 +1,7 @@
 #ifndef JUDGE_H
 #define JUDGE_H
-#include "../data/table.h"
 #include "../data/operation.h"
+#include "../data/table.h"
 
 Table *judge(int staffId, int *hasPermission, const char *password, Operation operation, Table *a);
 
